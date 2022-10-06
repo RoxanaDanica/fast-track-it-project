a343616bc6ba74833d62655351fd0bb13d4f293b
Roxi <roxiroxi135@yahoo.com>
2022-10-06 14:48:05 +0300

    Added styles for moviePage

--- src/Views/MoviePage.js
+++ src/Views/MoviePage.js
@@ -1,13 +1,17 @@
 import React from "react";
 
+import "../Styles/MoviePage.css";
+
 function MoviePage(props){
     return(
         <div className="MoviePage-div">
             <img className="MoviePage-img" src={props.movie.poster_path}/>
-            <h2>{props.movie.title}</h2>
-            <h5>{props.movie.release_date}</h5>
-            <h5>{props.movie.popularity}</h5>
-            <p>{props.movie.overview}</p>
+            <div className="MoviePage-text">
+                <h2>{props.movie.title}</h2>
+                <h5>{props.movie.release_date}</h5>
+                <h5>{props.movie.popularity}</h5>
+                <p>{props.movie.overview}</p>
+            </div>
         </div>
     )
 }
