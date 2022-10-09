35043ebdb076dffc34b365a8a3ebf4774a5a0c74
Roxi <roxiroxi135@yahoo.com>
2022-10-09 18:25:24 +0300

    Added loading page. Added fetch for header buttons.

--- src/App.js
+++ src/App.js
@@ -6,34 +6,61 @@ import MoviePage from "./Views/MoviePage";
 import SearchBar from "./Components/SearchBar";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import LoadingPage from "./Views/LoadingPage";
 
 import './App.css';
 
-import movies from './data/movies2.json';
+import movies from './data/movies.json';
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
+		'X-RapidAPI-Host': 'advanced-movie-search.p.rapidapi.com'
+	}
+};
 
 function App() {
   const [viewState, setViewState] = useState('TopPage');
   const [contentPage, setContentPage] = useState(<TopPage movies={movies.results} navigateToMovieFunction={navigateToMovie}/>);
+  const [foundMovies, setFoundMovies] = useState([]);
 
-  function changePage(newPage, movie) {
+  function changePage(newPage, elementsToDisplay) {
     setViewState(newPage);
 
     switch(newPage) {
       case 'TopPage' : 
-        setContentPage(<TopPage movies={movies.results} navigateToMovieFunction={navigateToMovie} />);
+        setContentPage(<TopPage movies={elementsToDisplay} navigateToMovieFunction={navigateToMovie} />);
         break;
       case 'MoviePage' :
-        setContentPage(<MoviePage movie={movie} />);
+        setContentPage(<MoviePage movie={elementsToDisplay} />);
+        break;
+      case 'LoadingPage' : 
+        setContentPage(<LoadingPage/>);
         break;
     }
   }
 
   function searchMovie(text) {
-    console.log("Searched text: " + text); 
+    changePage('LoadingPage');
+    fetch(`https://advanced-movie-search.p.rapidapi.com/search/movie?query=${text}&page=1`, options)
+      .then(response => response.json())
+      .then(response => {
+        setFoundMovies(response.results);
+        changePage("TopPage", response.results);
+      })
+      .catch(err => console.error(err));
   }
 
   function getMoviesByGenre(genreId) {
-    console.log("Showing movie by Id" + genreId);
+    changePage('LoadingPage');
+    fetch(`https://advanced-movie-search.p.rapidapi.com/discover/movie?with_genres=${genreId}&page=1`, options)
+      .then(response => response.json())
+      .then(response => {
+        setFoundMovies(response.results);
+        changePage("TopPage", response.results);
+      })
+      .catch(err => console.error(err));
   }
 
   function navigateToMovie(movie) {
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,10 +6,10 @@ function Header(props) {
 
     return(
         <div className="Header-div">
+           <button onClick={() => {props.getMoviesByGenreFunction(80)}}>Crime</button>
            <button onClick={() => {props.getMoviesByGenreFunction(28)}}>Action</button>
            <button onClick={() => {props.getMoviesByGenreFunction(12)}}>Adventure</button>
            <button onClick={() => {props.getMoviesByGenreFunction(35)}}>Comedy</button>
-           <button onClick={() => {props.getMoviesByGenreFunction(80)}}>Crime</button>
            <button onClick={() => {props.getMoviesByGenreFunction(18)}}>Drama</button>
         </div>
     )
